44116cdc5939bda5cd99dcef7c631a6ccb6526ed
Benedikt Boecking <benediktb@lena.auton.cs.cmu.edu>
2015-12-13 22:08:59 -0500

    fixed sign error in loss and loss gradient calculation

--- l_loss.m
+++ l_loss.m
@@ -1,6 +1,6 @@
 function v = l_loss(X,Y,Ytil,M,n)
     v = 0;
     for row = 1:n
-        v = v + log_one_x_exp(Ytil(row)-Y(row)*X(row,:)*M*X(row,:)');
+        v = v + log_one_x_exp(-Ytil(row)+Y(row)*X(row,:)*M*X(row,:)');
     end
 end
