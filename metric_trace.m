56e1987811c3f2745f10aa84314c0d3c1e681315
Benedikt Boecking <benediktb@lena.auton.cs.cmu.edu>
2015-12-14 10:52:12 -0500

    small bug fix

--- metric_trace.m
+++ metric_trace.m
@@ -1,17 +1,23 @@
-function [M,k] = metric_trace(X,Y,Ytil,stepsize,max_it,lm,eps)
+function [M,k,loss] = metric_trace(X,Y,Ytil,stepsize,max_it,lm,eps,M)
     [n,m] = size(X);
-    %initialize metric matrix M
-    M=zeros(m);
+    beta=0.1;
     loss_last = Inf;
     for k = 1:max_it
         V = l_grad(X,Y,Ytil,M,n,m);
-        M = prox_tr(M-stepsize*V,stepsize*lm);
-        %symmtrize since roundoff errors can lead to result not being symmetric
-        M = (M+M')/2;
-        loss = l_loss(X,Y,Ytil,M,n)+lm*sum(svd(M));
+        Mup = prox_tr(M-stepsize*V,stepsize*lm);
+        Mup = (Mup+Mup')/2;
+        loss = l_loss(X,Y,Ytil,Mup,n)+lm*sum(svd(Mup));
         if loss_last-loss<eps
+            if loss_last>loss
+                M=Mup;
+            else
+                %minimum overshot, set variables to previous value
+                loss = loss_last;
+                k=k-1;
+            end
             break
         end
+        M=Mup;
         loss_last = loss
     end 
 end
--- run.m
+++ run.m
@@ -1,4 +1,7 @@
 load('image_segmentation_data.mat')
 [n,m] =  size(pairwise_feature);
-[M num_iter] = metric_trace(pairwise_feature,pairwise_labels,ones(n),0.0001,10000,10,0.001)
+%run with biggest possible step size
+[M num_iter loss] = metric_trace(pairwise_feature,pairwise_labels,ones(n),10^-12,10000,10,0.001,zeros(m))
+%run again after convergence with smaller stepsize
+[M num_iter loss] = metric_trace(pairwise_feature,pairwise_labels,ones(n),10^-13,10000,10,0.001,M)
 [M num_iter] = metric_frob(pairwise_feature,pairwise_labels,ones(n),10^-14,10000,10,0.001)
